95323983932a7f84f26fb327f43cb9a2569ac71e
Calin Rafa <calin.rafa@evozon.com>
2019-09-05 17:33:04 +0300

    new design on quiz

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -86,21 +86,28 @@ class Quiz extends React.Component {
     }
 
     render() {
-        let questionNumber = this.state.questionNumber
         return (
             <div className="container">
-                <Link to="/">Home</Link>
+                <div className="d-flex justify-content-between mb-3 mt-3">
+                    <Link className="btn btn-outline-primary" to="/">Home</Link>
+                    <Link className="btn btn-outline-primary" to="/interviewQuiz/">Close Quiz</Link>
+                </div>
+
                 {
                     this.state.quizData.length > 0 &&
                     <>
                         <div className="row">
                             <div className="col-sm-12">
                                 <div>
-                                    <div className="progress">
-                                        <div className="progress-bar" role="progressbar"
-                                             style={{width: this.state.progress + "%"}}
-                                             aria-valuenow={this.state.progress} aria-valuemin="0"
-                                             aria-valuemax="100">{this.state.progress}%
+                                    <div className="progress mb-5" style={{height: "20px"}}>
+                                        <div
+                                            className={"progress-bar progress-bar-striped progress-bar-animated " + (this.state.done === true ? "bg-success" : "bg-warning")}
+                                            role="progressbar"
+                                            style={{width: this.state.progress + "%", fontWeight: "bold"}}
+                                            aria-valuenow={this.state.progress}
+                                            aria-valuemin="0"
+                                            aria-valuemax="100">
+                                            {this.state.progress}%
                                         </div>
                                     </div>
                                 </div>
@@ -109,34 +116,54 @@ class Quiz extends React.Component {
                         </div>
                         <div className="row">
                             <div className="col-sm-12">
-                                <div>{this.state.quizData[this.state.questionNumber].id}</div>
-                                <div>{this.state.quizData[this.state.questionNumber].question}</div>
+                                <h4 className="text-center mb-4">{this.state.quizData[this.state.questionNumber].question}</h4>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-sm-12">
-                                <div>
-                                    <div>
-                                          <textarea onChange={this.handleTextArea}
-                                                    value={this.state.quizAnswers[this.state.questionNumber + 1] ? this.state.quizAnswers[this.state.questionNumber + 1] : ""}
-                                                    cols={40} rows={10}/>
+                            <div className="col-sm-12 d-flex justify-content-center mb-5">
+                                <div style={{width: "600px"}}>
+                                    <div className="mb-3">
+                                        <div className="form-group">
+                                            <label htmlFor="response-text-area">Response!</label>
+                                            <textarea onChange={this.handleTextArea}
+                                                      value={this.state.quizAnswers[this.state.questionNumber + 1] ? this.state.quizAnswers[this.state.questionNumber + 1] : ""}
+                                                      className="form-control" id="response-text-area" rows="3"/>
+                                        </div>
                                     </div>
-                                    <div>
-                                          <textarea onChange={this.handleExamplesTextArea}
-                                                    value={this.state.quizExamples[this.state.questionNumber + 1] ? this.state.quizExamples[this.state.questionNumber + 1] : ""}
-                                                    cols={40} rows={10}/>
+                                    <div className="mb-3">
+                                        <div className="form-group">
+                                            <label htmlFor="example-text-area">Give a example if needed!</label>
+                                            <textarea onChange={this.handleExamplesTextArea}
+                                                      value={this.state.quizExamples[this.state.questionNumber + 1] ? this.state.quizExamples[this.state.questionNumber + 1] : ""}
+                                                      className="form-control" id="example-text-area" rows="3"/>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <button onClick={this.getPrevQuestion}>Prev Question</button>
-                                        <button onClick={this.getNextQuestion}>Next Question</button>
+                                    <div className="mb-5">
+                                        {
+                                            this.state.questionNumber > 0 &&
+                                            <button className="btn btn-outline-primary float-left"
+                                                    onClick={this.getPrevQuestion}>Prev
+                                                Question
+                                            </button>
+                                        }
+                                        {
+                                            this.state.questionNumber < this.state.quizData.length - 1 &&
+                                            <button className="btn btn-outline-primary float-right"
+                                                    onClick={this.getNextQuestion}>Next
+                                                Question
+                                            </button>
+                                        }
+
                                     </div>
                                 </div>
                             </div>
                             <div className="col-sm-12">
                                 {
                                     this.state.done === true &&
-                                    <div>
-                                        <button onClick={this.finishQuiz}>Finish Quiz!</button>
+                                    <div className="d-flex justify-content-center">
+                                        <button className="btn btn-outline-success btn-lg" onClick={this.finishQuiz}>See
+                                            Results!
+                                        </button>
                                     </div>
                                 }
                             </div>
