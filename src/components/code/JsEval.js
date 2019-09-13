e611cf83297934f59264a1ec6a3aba026e2ae80e
rafazor <rafa.calin@yahoo.com>
2019-09-13 09:10:20 +0300

    eval fixes

--- src/components/code/JsEval.js
+++ src/components/code/JsEval.js
@@ -2,7 +2,11 @@ import React from "react"
 
 
 class JsEval extends React.Component {
-
+    state = {
+        codeResult: "",
+        expectedResult: "",
+        evalMessage: ""
+    }
     evalCode = (code) => {
         try {
             return eval(code);
@@ -13,13 +17,39 @@ class JsEval extends React.Component {
         }
     }
 
+    checkSolution = (codeResult) => {
+        if (codeResult == this.props.algorithmAnswer) {
+            return "good"
+        } else {
+            return "not good"
+
+        }
+    }
+    handleShowCheckSolution = () => {
+        let codeResult = this.evalCode(this.props.codeToEval)
+        this.setState({
+            codeResult: codeResult.toString(),
+            expectedResult: this.props.algorithmAnswer,
+            evalMessage: this.checkSolution(codeResult)
+
+        })
+    }
+
     render() {
         return (
             <>
-                <div>{this.evalCode(this.props.codeToEval)}</div>
-                <div>{"expected answer: " + this.props.algorithmAnswer}</div>
-
-                <div>{this.evalCode(this.props.codeToEval) == this.props.algorithmAnswer ? "GOOD" : "PLM"}</div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <button onClick={this.handleShowCheckSolution} className="btn btn-outline-primary ">Check
+                            Solution
+                        </button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">{this.state.codeResult}</div>
+                    <div className="col-md-4">{this.state.expectedResult}</div>
+                    <div className="col-md-4">{this.state.evalMessage}</div>
+                </div>
             </>
         )
     }
