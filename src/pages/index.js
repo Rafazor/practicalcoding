5170d328f8591a97b54955d3dc23c84a1aab52e6
Calin Rafa <calin.rafa@evozon.com>
2019-09-26 16:29:29 +0300

    changed the meta description!

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,11 @@ class Index extends React.Component {
                 <Helmet>
                     <meta charSet="utf-8"/>
                     <title>Online Coding Books</title>
-                    <meta name="description" content="HTML5, CSS3, JavaScript,
-ECMAScript and Algorithms"/>
+                    <meta name="description"
+                          content="Enhance Your Knowledge! We selected all the top interview topics that are relevant in 2019 and 2020 for a JavaScript and Front End developer. Test your technical interview skills with our programming questions and algorithms!"/>
                     <link rel="canonical" href="https://onlinecodingbooks.com/"/>
                     <meta name="google-site-verification" content="6cnnzMfAqov9B9Tvj9iqKPcXGbAklNilQsFWBu-3IcU"/>
-                    <meta name="msvalidate.01" content="3DFC34FFFA6A9D0C795E0E2F28E85C6A" />
+                    <meta name="msvalidate.01" content="3DFC34FFFA6A9D0C795E0E2F28E85C6A"/>
                 </Helmet>
                 <StartContent/>
             </Layout>
